5e36e470d8fc412dda8741bbe7f84dbe1b87dd5d
TareqJudehGithub <tareq.joudeh@gmail.com>
2021-10-17 17:05:01 +0200

    Successfully completed Week2 lessons, exercises, and main objectives.

---
+++ week2/args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(int argc, str argv[])
+{
+    
+}
+
+/* 
+argc: argument count: how much words the user typed (includes compiler file name)
+    $ ./file hello world  => argc = 3
+
+argv[]:  v for vector (a list or an array)
+argv[0] is the name of the program (filename)
+argv[1] is the word right after the program.. and so fourth
+
+*/
